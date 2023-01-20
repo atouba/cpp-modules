cfa0befc2317a41180acc6232cdc68185552ff41
atouba <anasstouba5@gmail.com>
2023-01-20 11:10:08 +0100

    delete useless lines

--- cpp1/ex05/main.cpp
+++ cpp1/ex05/main.cpp
@@ -7,10 +7,6 @@
 
 int main() {
 	Harl	one;
-	std::string a = "DEBUG";
-	std::string b = "INFO";
-	std::string c = "WARNING";
-	std::string d = "ERROR";
 
 	one.complain("ERROR");
 	one.complain("WARNING");
