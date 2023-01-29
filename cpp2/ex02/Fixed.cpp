d0ef7b0f536901da4130f14bdf3043d35b8f9e78
anass touba <anasstouba5@gmail.com>
2023-01-30 00:17:21 +0100

    ex02

---
+++ cpp2/ex02/Fixed.cpp
@@ -0,0 +1,93 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed():_integer(0) {std::cout << "Default constructor called" << std::endl;}
+
+Fixed::Fixed(const int integer): _integer(integer * 256) {
+    std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float number): _integer(number * 256) {
+    std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const & f) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = f;
+}
+
+Fixed&  Fixed::operator=( Fixed const& f) {
+    std::cout << "Copy assignment operator called" << std::endl;
+   _integer = f._integer;
+    return *this;
+}
+
+Fixed::~Fixed() {std::cout << "Destructor called" << std::endl;}
+
+int Fixed::getRawBits(void) const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return _integer;
+}
+
+void    Fixed::setRawBits(int const raw) {
+    std::cout << "setRawBits member function called" << std::endl;
+    _integer = raw;
+}
+
+int Fixed::toInt() const {
+    int original_int = _integer / 256;
+    return original_int;
+}
+
+float   Fixed::toFloat() const {
+    float   original_float = (float)_integer / 256;
+    return original_float;
+}
+
+std::ostream&	operator<<(std::ostream& o, const Fixed& f) {
+    o << f.toFloat();
+    return o;
+}
+
+bool	Fixed::operator>(const Fixed& f) const {
+    return this->getRawBits() > f.getRawBits();
+}
+
+bool	Fixed::operator<(const Fixed& f) const {
+    return this->getRawBits() < f.getRawBits();
+}
+
+bool	Fixed::operator>=(const Fixed& f) const {
+    return this->getRawBits() >= f.getRawBits();
+}
+
+bool	Fixed::operator<=(const Fixed& f) const {
+    return this->getRawBits() <= f.getRawBits();
+}
+
+bool	Fixed::operator==(const Fixed& f) const {
+    return this->getRawBits() == f.getRawBits();
+}
+
+bool	Fixed::operator!=(const Fixed& f) const {
+    return this->getRawBits() != f.getRawBits();
+}
+
+float	Fixed::operator+(const Fixed& f) const {
+    float   res = (float)(this->getRawBits() + f.getRawBits()) / 256;
+    return res;
+}
+
+float	Fixed::operator-(const Fixed& f) const {
+    float   res = (float)(this->getRawBits() - f.getRawBits()) / 256;
+    return res;
+}
+unfinished work
+float	Fixed::operator*(const Fixed& f) const {
+    float   res = (float)(this->getRawBits() * f.getRawBits()) / 256;
+    return res;
+}
+
+float	Fixed::operator/(const Fixed& f) const {
+    float   res = (float)(this->getRawBits() / f.getRawBits()) / 256;
+    return res;
+}
---
+++ cpp2/ex02/Fixed.hpp
@@ -0,0 +1,38 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+private:
+	int					_integer;
+	static const int	_fractional = 8;
+
+public:
+	Fixed();
+	Fixed(const Fixed& f);
+	Fixed&	operator=(const Fixed& f);
+	~Fixed();
+
+	int		getRawBits( void ) const;
+	void	setRawBits(int const raw);
+	Fixed(const int integer);
+	Fixed(const float number);
+	int toInt( void ) const;
+	float toFloat( void ) const;
+
+	bool	operator>(const Fixed& f) const;
+	bool	operator<(const Fixed& f) const;
+	bool	operator>=(const Fixed& f) const;
+	bool	operator<=(const Fixed& f) const;
+	bool	operator==(const Fixed& f) const;
+	bool	operator!=(const Fixed& f) const;
+	float	operator+(const Fixed& f) const;
+	float	operator-(const Fixed& f) const;
+	float	operator*(const Fixed& f) const;
+	float	operator/(const Fixed& f) const;
+};
+
+std::ostream&	operator<<(std::ostream& o, const Fixed& f);
+
+#endif
